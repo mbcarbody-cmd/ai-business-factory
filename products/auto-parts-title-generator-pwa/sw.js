480d5287172e786d5b357128497cb632e2b84a76
mbcarbody-cmd <mbcarbody@gmail.com>
2026-06-14 15:41:20 +0300

    Add service worker for auto parts title generator

---
+++ products/auto-parts-title-generator-pwa/sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open('parts-title-generator-v1').then(cache => cache.addAll(['./index.html','./manifest.json'])));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
