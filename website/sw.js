c602b233d34235eee9f700c71257f56388083ad9
mbcarbody-cmd <mbcarbody@gmail.com>
2026-06-17 20:52:41 +0300

    Cache the real video maker PWA

--- website/sw.js
+++ website/sw.js
@@ -1,30 +1,23 @@
-const CACHE = 'content-hook-factory-v1';
+const CACHE = 'quick-product-video-v2';
 const APP_SHELL = [
   './android.html',
-  './index.html',
+  './video-maker.html',
   './manifest.webmanifest',
   './icon.svg'
 ];
-
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match('./android.html')))
-  );
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match('./video-maker.html'))));
 });
